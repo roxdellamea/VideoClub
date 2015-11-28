6e5f7535ad2fc45e9977e001e5610f4900a130ef
roxdellamea <roxdellamea@gmail.com>
2015-11-28 12:22:43 -0300

    registro y login

--- src/VideoClubBundle/Controller/MovieController.php
+++ src/VideoClubBundle/Controller/MovieController.php
@@ -14,45 +14,4 @@ class MovieController extends Controller
     {
         return $this->render('VideoClubBundle:Movie:index.html.twig');
     }
-
-    public function listAction($val)
-    {
-       	$em = $this->getDoctrine()->getManager();
-
-    	$movies = $em->getRepository('VideoClubBundle:Movie')->findByGender($val);
-
-    	return $this->render('VideoClubBundle:Movie:list.html.twig', array('movies' => $movies));
-    	
-	}
-
-    public function addAction()
-    {
-        $movie = new Movie();
-        $form = $this->createCreateForm($movie);
-
-        return $this->render('VideoClubBundle:Movie:add.html.twig', array('form' => $form->createView()));
-    }
-
-    private function createCreateForm(Movie $entity)
-    {
-        $form = $this->createForm(new MovieType(), $entity, array('action' => $this->generateUrl('vc_movie_create'), 'method' => 'POST'));
-        return $form;
-    }
-	
-    public function createAction(Request $request)
-    {
-        $movie = new Movie();
-        $form = $this->createCreateForm($movie);
-        $form->handleRequest($request);
-
-        if($form->isValid())
-        {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($movie);
-            $em->flush();
-
-            return $this->redirectToRoute('vc_movie_add');
-        }
-        return $this->render('VideoClubBundle:Movie:add.html.twig', array('form' => $form->createView()));
-    }
 }
--- src/VideoClubBundle/VideoClubBundle.php
+++ src/VideoClubBundle/VideoClubBundle.php
@@ -6,4 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class VideoClubBundle extends Bundle
 {
+	public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
 }
