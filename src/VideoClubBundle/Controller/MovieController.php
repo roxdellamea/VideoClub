ceb7c92b897489740e9caf466489c84e593b92e2
roxdellamea <roxdellamea@gmail.com>
2015-12-02 01:29:55 -0300

    compra de pack y alquiler de peliculas

--- src/VideoClubBundle/Controller/MovieController.php
+++ src/VideoClubBundle/Controller/MovieController.php
@@ -6,12 +6,56 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use VideoClubBundle\Entity\Movie;
+use VideoClubBundle\Entity\Rental;
+use VideoClubBundle\Entity\User;
 use VideoClubBundle\Form\MovieType;
 
 class MovieController extends Controller
 {
     public function indexAction()
-    {
+    {	
         return $this->render('VideoClubBundle:Movie:index.html.twig');
+    }
+
+    public function listAction($val)
+    {
+       	$em = $this->getDoctrine()->getManager();
+
+    	$movies = $em->getRepository('VideoClubBundle:Movie')->findByGender($val);
+
+    	return $this->render('VideoClubBundle:Movie:list.html.twig', array('movies' => $movies));
+    }
+
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $request = $this->getRequest();
+        $sesion = $request->getSession();
+
+        $fosUser = $this->get('security.context')->getToken()->getUser();
+        if ($fosUser != 'anon.') {
+            $user = $em->getRepository('VideoClubBundle:User')->find($fosUser->getId());
+        } else {
+            return $this->redirect($this->generateUrl('fos_user_security_login'));
+        }
+
+        $movie = $em->getRepository('VideoClubBundle:Movie')->find($id);
+
+        if (!$movie) {
+            throw $this->createNotFoundException('Unable to find Movie entity.');
+        }
+
+        if ($user->getFreemin() >= $movie->getRuntime()) {
+            $rental = new Rental();
+            $rental->setUser($user);
+            $rental->setMovie($movie);
+            $user->setFreemin($user->getFreemin() - $movie->getRuntime());
+            $em->persist($rental);
+            $em->persist($user);
+            $em->flush();
+            return $this->render('VideoClubBundle:Movie:show.html.twig', array('movie' => $movie));
+        }else{
+            return $this->render('VideoClubBundle:Movie:msn.html.twig');
+        }
     }
 }
--- src/VideoClubBundle/Controller/PackageMinController.php
+++ src/VideoClubBundle/Controller/PackageMinController.php
@@ -4,16 +4,57 @@ namespace VideoClubBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use VideoClubBundle\Entity\User;
+use VideoClubBundle\Entity\PurchasePack;
 
 class PackageMinController extends Controller
 {
 	public function indexAction()
-	    {
-	       	$em = $this->getDoctrine()->getManager();
+	{
+	    $em = $this->getDoctrine()->getManager();
 
-	    	$packs = $em->getRepository('VideoClubBundle:PackageMin')->findAll();
+	   	$packs = $em->getRepository('VideoClubBundle:PackageMin')->findAll();
 
-	    	return $this->render('VideoClubBundle:PackageMin:index.html.twig', array('packs' => $packs));
+	    return $this->render('VideoClubBundle:PackageMin:index.html.twig', array('packs' => $packs));
 	    	
-		}
+	}
+
+	public function buyAction($id)
+	{
+		$em = $this->getDoctrine()->getManager();
+		$request = $this->getRequest();
+        $sesion = $request->getSession();
+
+        $fosUser = $this->get('security.context')->getToken()->getUser();
+        if ($fosUser != 'anon.') {
+            $user = $em->getRepository('VideoClubBundle:User')->find($fosUser->getId());
+        } else {
+            return $this->redirect($this->generateUrl('fos_user_security_login'));
+        }
+
+		$packMin = $em->getRepository('VideoClubBundle:PackageMin')->find($id);
+
+        if (!$packMin) {
+            throw $this->createNotFoundException('Unable to find PackageMin entity.');
+        }
+
+        $purchasePack = new PurchasePack();
+        $purchasePack->setUser($user);
+        $purchasePack->setPackagemin($packMin);
+        $purchasePack->setTotalcost($packMin->getCost());
+
+        $min = $packMin->getMinutes();
+
+        $user->setFreemin($user->getFreemin() + $min);
+
+
+        $em->persist($purchasePack);
+        $em->persist($user);
+        $em->flush();
+
+        return $this->render('VideoClubBundle:Movie:index.html.twig');
+    }
 }
+
+
+  
